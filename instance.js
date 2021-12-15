800c099c3b5a4207ad4e072f0d2c0cefcbd27f11
Ibnu Fajar Yunardi <ifyunardi@binar.co.id>
2021-12-15 10:59:49 +0700

    kalibrasi 15des21

--- instance.js
+++ instance.js
@@ -2,9 +2,9 @@ const c = (module.exports = {});
 
 c.symbols = [
   {
-    symbol: 'SANDUSDT',
+    symbol: 'MANAUSDT',
     exchange: 'binance_futures',
-    periods: ['5m', '15m', '1h'],
+    periods: ['3m', '15m', '1h'],
     trade: {
       currency_capital: 400,
       strategies: [
@@ -20,17 +20,17 @@ c.symbols = [
     watchdogs: [
       {
         name: 'risk_reward_ratio',
-        target_percent: 2.5,
+        target_percent: 3.0,
         stop_percent: 10.0
       }
     ]
   },
   {
-    symbol: 'AXSUSDT',
+    symbol: 'GALAUSDT',
     exchange: 'binance_futures',
-    periods: ['5m', '15m', '1h'],
+    periods: ['3m','15m', '1h'],
     trade: {
-      capital: 2,
+      currency_capital: 400,
       strategies: [
         {
           strategy: 'dip_catcher',
@@ -44,7 +44,31 @@ c.symbols = [
     watchdogs: [
       {
         name: 'risk_reward_ratio',
-        target_percent: 2.5,
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'MATICUSDT',
+    exchange: 'binance_futures',
+    periods: ['3m', '15m', '1h'],
+    trade: {
+      currency_capital: 400,
+      strategies: [
+        {
+          strategy: 'dip_catcher',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
         stop_percent: 10.0
       }
     ]
